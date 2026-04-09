42a40ca8a80ea1a5c26ae6911b1e14f2c98a8035
Imagekafle10 <hpimage10@gmail.com>
2026-04-09 15:51:58 +0545

    Created: Route Params

--- my-nest-api/src/users/controllers/users/users.controller.ts
+++ my-nest-api/src/users/controllers/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req, Res } from '@nestjs/common';
 import type { Request, Response } from 'express';
 import { title } from 'process';
 import { CreateUserDto } from '../../dtos/CreateUser.dtos';
@@ -34,5 +34,11 @@ export class UsersController {
   createUser(@Body() userData: CreateUserDto) {
     console.log(userData.username);
     return 'Created';
+  }
+
+  @Get(':id/:postId')
+  getUserById(@Param('id') id: string, @Param('postId') postId: string) {
+    console.log(id);
+    return { id, postId };
   }
 }
