37832f22f06756182445ffdc4660356b6cd8ee7d
Imagekafle10 <hpimage10@gmail.com>
2026-04-14 23:36:52 +0545

    Controlling Req and Response

--- nestjs_core/src/customers/controllers/customers/customers.controller.ts
+++ nestjs_core/src/customers/controllers/customers/customers.controller.ts
@@ -1,11 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { CustomersService } from '../../services/customers/customers.service';
+import type { Request, Response } from 'express';
 
 @Controller('customers')
 export class CustomersController {
   constructor(private customersService: CustomersService) {}
-  @Get('')
-  getCustomer() {
-    return this.customersService.findCustomers();
+  @Get(':id')
+  getCustomer(
+    @Param('id', ParseIntPipe) id: number,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    console.log(typeof id);
+
+    const customer = this.customersService.findCustomersById(id);
+    if (customer) {
+      res.json(customer);
+    } else {
+      res.status(400).json({ message: 'Customer not Found' });
+    }
+  }
+
+  @Get('/search/:id')
+  searchCustomerById(@Param('id', ParseIntPipe) id: number) {
+    const customer = this.customersService.findCustomersById(id);
+    if (customer) return customer;
+    else throw new HttpException('Customer Not Found', HttpStatus.NOT_FOUND);
   }
 }
