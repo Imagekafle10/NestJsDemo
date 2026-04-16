7cd20bde09866391edd1b4ce686d0def0167173e
Imagekafle10 <hpimage10@gmail.com>
2026-04-16 12:47:54 +0545

    Validate: Post Request

--- nestjs_core/src/customers/dtos/CreateCustomer.dto.ts
+++ nestjs_core/src/customers/dtos/CreateCustomer.dto.ts
@@ -1,5 +1,27 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNotEmptyObject,
+  IsNumberString,
+  ValidateNested,
+} from 'class-validator';
+import { CreateAddressDto } from './CreateAddress.dto';
+import { Type } from 'class-transformer';
+
 export class CreateCustomerDto {
+  @IsEmail()
   email!: string;
+
+  @IsNumberString()
+  @IsNotEmpty()
   id!: number;
+
+  @IsNotEmpty()
   name!: string;
+
+  //Validation for Nested Object
+  @ValidateNested()
+  @IsNotEmptyObject()
+  @Type(() => CreateAddressDto)
+  address!: CreateAddressDto;
 }
